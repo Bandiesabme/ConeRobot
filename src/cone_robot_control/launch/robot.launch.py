8f3163bfc418c90bd5310f7cead6b0e3c8044ee1
Lernyei András <andras.lernyei@edu.bme.hu>
2026-08-20 18:45:07 +0200

    tf enable

--- src/cone_robot_control/launch/robot.launch.py
+++ src/cone_robot_control/launch/robot.launch.py
@@ -119,7 +119,7 @@ def launch_setup(context, *args, **kwargs):
                     parameters=[{
                         'laser_scan_topic': '/scan',
                         'odom_topic': '/odom',
-                        'publish_tf': False,
+                        'publish_tf': True,
                         'base_frame_id': 'base_link',
                         'odom_frame_id': 'odom',
                         'laser_frame_id': 'laser_frame',
